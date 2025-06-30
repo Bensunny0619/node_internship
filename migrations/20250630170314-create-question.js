1532a8f112358b6e9be7476a3d7d7312d8477e33
Gbenga Emmanuel <98877287+Bensunny0619@users.noreply.github.com>
2025-06-30 18:07:48 +0100

    Initial quiz setup

---
+++ migrations/20250630170314-create-question.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      text: {
+        type: Sequelize.STRING
+      },
+      options: {
+        type: Sequelize.JSON
+      },
+      answer: {
+        type: Sequelize.STRING
+      },
+      quizId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Questions');
+  }
+};
---
+++ models/question.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Question extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Question.init({
+    text: DataTypes.STRING,
+    options: DataTypes.JSON,
+    answer: DataTypes.STRING,
+    quizId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Question',
+  });
+  return Question;
+};
